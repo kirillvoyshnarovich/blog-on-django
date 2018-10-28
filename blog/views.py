d4860ab25aa27a52f58a80f31e078ff21209f353
kirillvoyshnarovich <kirill.voyshnarovich@gmail.com>
2018-10-29 01:36:07 +0300

    Modified templates to display posts from datbase

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 # Create your views here.
